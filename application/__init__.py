95ed48d809c8aff9b16d406d5a83dd3ac616be9d
deadmedusa <60216330+deadmedusa@users.noreply.github.com>
2020-03-10 11:02:54 +0000

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,9 +5,9 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = getenv('VGR_SECRET_KEY')
+app.config['SECRET_KEY'] = getenv('DINER_SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('VGR_DB_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DINER_DB_URI'))
 db = SQLAlchemy(app)
 
 from application import routes
