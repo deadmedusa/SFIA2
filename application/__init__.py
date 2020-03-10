3d2add3047b111814dd19f72547e6732d6836939
deadmedusa <60216330+deadmedusa@users.noreply.github.com>
2020-03-10 12:01:43 +0000

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,9 @@ from os import getenv
 
 app = Flask(__name__)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 app.config['SECRET_KEY'] = getenv('DINER_SECRET_KEY')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DINER_DB_URI'))
